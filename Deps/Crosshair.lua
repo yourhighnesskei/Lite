760c236cb45543b0afc70bb7c42a842c701da985
Kei <personalchess79@gmail.com>
2026-03-30 15:02:32 +0700

    Add files via upload

--- Deps/Crosshair.lua
+++ Deps/Crosshair.lua
@@ -1,27 +1,22 @@
 local Drawing = loadstring(game:HttpGet('https://raw.githubusercontent.com/yourhighnesskei/Lite/refs/heads/main/Deps/Drawing.lua'))()
 
 getgenv().crosshair = {
-
     enabled = true,
     refreshrate = 0.015,
-    mode = 'Mouse', -- Middle, Mouse, Custom
-    position = Vector2.new(0,0), -- custom position
-
+    mode = 'Mouse',
+    position = Vector2.new(0,0),
     width = 2.5,
     length = 10,
     radius = 11,
     color = Color3.fromRGB(66, 84, 245),
-
-    spin = true, -- animate the rotation
+    spin = true,
     spin_speed = 150,
     spin_max = 340,
-    spin_style = Enum.EasingStyle.Circular, -- Linear for normal smooth spin
-
-    resize = true, -- animate the length
+    spin_style = Enum.EasingStyle.Circular,
+    resize = true,
     resize_speed = 150,
     resize_min = 5,
     resize_max = 22,
-
 }
 
 local runservice = game:GetService('RunService')
@@ -34,10 +29,22 @@ local last_render = 0
 local drawings = {
     crosshair = {},
     text = {
-        Drawing.new('Text', {Size = 14, Font = 2, Outline = true, Center = true, Text = 'Alwayswin', Color = Color3.new(1,1,1)}),
-        Drawing.new('Text', {Size = 14, Font = 2, Outline = true, Center = true, Text = '.lite'}),
+        Drawing.new('Text'),
+        Drawing.new('Text')
     }
 }
+
+drawings.text[1].Size = 14
+drawings.text[1].Font = 2
+drawings.text[1].Outline = true
+drawings.text[1].Text = "Alwayswin"
+drawings.text[1].Color = Color3.new(1,1,1)
+
+drawings.text[2].Size = 14
+drawings.text[2].Font = 2
+drawings.text[2].Outline = true
+drawings.text[2].Text = ".lite"
+drawings.text[2].Color = Color3.fromRGB(66, 84, 245)
 
 for idx = 1, 4 do
     drawings.crosshair[idx] = Drawing.new('Line')
@@ -52,7 +59,6 @@ function solve(angle, radius)
 end
 
 runservice.PostSimulation:Connect(function()
-
     local _tick = tick()
 
     if _tick - last_render > crosshair.refreshrate then
@@ -71,40 +77,39 @@ runservice.PostSimulation:Connect(function()
         text_2.Visible = crosshair.enabled
 
         if crosshair.enabled then
-
-            local text_x = text_1.TextBounds.X + text_2.TextBounds.X
-
-            text_1.Position = position + Vector2.new(0, crosshair.radius + (crosshair.resize and crosshair.resize_max or crosshair.length) + 15)
-            text_2.Position = position + Vector2.new(0, crosshair.radius + (crosshair.resize and crosshair.resize_max or crosshair.length) + 30)
+            local text_x = text_1.TextBounds.X + text_2.TextBounds.X
+
+            text_1.Position = position + Vector2.new(-text_x / 2, crosshair.radius + (crosshair.resize and crosshair.resize_max or crosshair.length) + 15)
+            text_2.Position = text_1.Position + Vector2.new(text_1.TextBounds.X)
             text_2.Color = crosshair.color
-            
+
             for idx = 1, 4 do
                 local outline = drawings.crosshair[idx]
                 local inline = drawings.crosshair[idx + 4]
-    
+
                 local angle = (idx - 1) * 90
                 local length = crosshair.length
-    
+
                 if crosshair.spin then
                     local spin_angle = -_tick * crosshair.spin_speed % crosshair.spin_max
                     angle = angle + tweenservice:GetValue(spin_angle / 360, crosshair.spin_style, Enum.EasingDirection.InOut) * 360
                 end
-    
+
                 if crosshair.resize then
                     local resize_length = tick() * crosshair.resize_speed % 180
                     length = crosshair.resize_min + math.sin(math.rad(resize_length)) * crosshair.resize_max
                 end
-    
+
                 inline.Visible = true
                 inline.Color = crosshair.color
                 inline.From = position + solve(angle, crosshair.radius)
                 inline.To = position + solve(angle, crosshair.radius + length)
                 inline.Thickness = crosshair.width
-    
+
                 outline.Visible = true
                 outline.From = position + solve(angle, crosshair.radius - 1)
                 outline.To = position + solve(angle, crosshair.radius + length + 1)
-                outline.Thickness = crosshair.width + 1.5    
+                outline.Thickness = crosshair.width + 1.5
             end
         else
             for idx = 1, 4 do
@@ -114,5 +119,3 @@ runservice.PostSimulation:Connect(function()
         end
     end
 end)
-
-return crosshair
